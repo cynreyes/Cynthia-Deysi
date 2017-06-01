9fd95ffa6a87a3c30e744dc73879efb125d485b6
cynreyes <cyreyes@cristoreyny.org>
2017-06-01 12:01:53 +0000

    commit

--- script.js
+++ script.js
@@ -18,16 +18,6 @@ console.log(url11)
             var venue=response.response.venues[i];
              $("#results").append(h4); 
             }
-            
-           
- /*           
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}  */      
         
         }); 
     });
